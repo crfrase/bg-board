0dcc886430c4189a9864c73b2f3e42876e995741
Cory <Cory@DESKTOP-XXXx>
2021-03-15 13:17:40 -0400

    end of lab 6

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -10,8 +10,20 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    MatCardModule, StoreUiSharedModule
+    RouterModule.forRoot(
+      [
+        {
+          path: 'game/:id',
+          loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(
+              (module) => module.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabled' }
+    ),
+    MatCardModule,
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/libs/store/ui-shared',
     '<rootDir>/libs/store/util-formatter',
     '<rootDir>/libs/store/util-formatters',
+    '<rootDir>/libs/store/feature-game-detail',
   ],
 };
