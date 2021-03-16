58498e49a202e3702faab8b5b6e550c593c2718a
Cory <Cory@DESKTOP-XXXx>
2021-03-16 12:02:22 -0400

    lab 14 bonus1

--- tools/generators/sort-project-references/index.ts
+++ tools/generators/sort-project-references/index.ts
@@ -1,4 +1,5 @@
 import { Tree, formatFiles, updateJson } from '@nrwl/devkit';
+const JSON5 = require('json5')
 
 export default async function (host: Tree, schema: any) {
   updateJson(host, "workspace.json", (json) => {
@@ -11,6 +12,18 @@ export default async function (host: Tree, schema: any) {
     json.projects = sortObjectKeys(json.projects);
     return json;
   });
+
+  updateJson(host, "tsconfig.base.json", (json) => {
+    json.compilerOptions.paths = sortObjectKeys(json.compilerOptions.paths);
+    return json;
+  });
+
+  const jestStr = host.read("jest.config.js");
+  let jsonPart = jestStr.subarray(jestStr.indexOf("{"), jestStr.lastIndexOf("}")+1).toString();
+  const moduleExports = JSON5.parse(jsonPart);;
+  moduleExports.projects = moduleExports.projects.sort();
+  //host.write("jest.config.js", `module.exports = ${JSON5.stringify(moduleExports)};`);
+
 
   await formatFiles(host);
 }
