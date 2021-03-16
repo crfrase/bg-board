687db3f5613d728737d6388c63043a2cb7ab4a13
Cory <Cory@DESKTOP-XXXx>
2021-03-16 10:28:16 -0400

    lab 13 midpoint

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,6 +2,12 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (host: Tree, schema: any) {
+  if (!schema.name.startsWith("util-")) {
+    schema.name = "util-" + schema.name;
+  }
+
+  schema.linter = "tslint";
+  schema.tags = "type:util,scope:" + schema.directory;
   await libraryGenerator(host, { name: schema.name });
   await formatFiles(host);
   return () => {
