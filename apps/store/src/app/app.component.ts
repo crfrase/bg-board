995e377447735d0274b1b0fb46ab91f7e1e7b8ff
Cory <Cory@DESKTOP-XXXx>
2021-03-16 16:27:06 -0400

    all labs

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Game } from '@bg-hoard/util-interface';
 })
 export class AppComponent {
   constructor(private http: HttpClient, @Inject('baseUrl') private baseUrl: string) {
-    console.log('componentss constructed');
+    console.log('componentsss constructed');
   }
 
   title = 'Board Game Hoard';
